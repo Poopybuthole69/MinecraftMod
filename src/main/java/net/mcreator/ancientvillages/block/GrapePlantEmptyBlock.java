1c2db7a9eecb3e5a91f34226d5b24205f5453ea3
HarmKraats <harmk@192.168.2.47>
2021-05-03 17:03:57 +0200

    Error's  fixed

--- src/main/java/net/mcreator/ancientvillages/block/GrapePlantEmptyBlock.java
+++ src/main/java/net/mcreator/ancientvillages/block/GrapePlantEmptyBlock.java
@@ -1,17 +1,40 @@
 
 package net.mcreator.ancientvillages.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.potion.Effects;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.ancientvillages.AncientVillagesModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @AncientVillagesModElements.ModElement.Tag
 public class GrapePlantEmptyBlock extends AncientVillagesModElements.ModElement {
-
 	@ObjectHolder("ancient_villages:grape_plant_empty")
 	public static final Block block = null;
-
 	public GrapePlantEmptyBlock(AncientVillagesModElements instance) {
 		super(instance, 13);
-
 	}
 
 	@Override
@@ -25,9 +48,7 @@ public class GrapePlantEmptyBlock extends AncientVillagesModElements.ModElement 
 	public void clientLoad(FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
 	}
-
 	public static class BlockCustomFlower extends FlowerBlock {
-
 		public BlockCustomFlower() {
 			super(Effects.SATURATION, 0, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().sound(SoundType.SWEET_BERRY_BUSH)
 					.hardnessAndResistance(0f, 0f).speedFactor(0.8f).setLightLevel(s -> 0));
@@ -46,7 +67,6 @@ public class GrapePlantEmptyBlock extends AncientVillagesModElements.ModElement 
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
@@ -57,7 +77,5 @@ public class GrapePlantEmptyBlock extends AncientVillagesModElements.ModElement 
 		public PlantType getPlantType(IBlockReader world, BlockPos pos) {
 			return PlantType.PLAINS;
 		}
-
 	}
-
 }
