76b3c593e6914397b6d52c288bd71c512418063a
Poopybuthole69 <Martijn@192.168.2.46>
2021-05-03 19:51:17 +0200

    Vilager 2 biomes added fride recipe added vileger dimensionadded

---
+++ src/main/java/net/mcreator/ancientvillages/world/biome/VillagerBiomeBiome.java
@@ -0,0 +1,245 @@
+
+package net.mcreator.ancientvillages.world.biome;
+
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.BiomeManager;
+
+import net.minecraft.world.gen.trunkplacer.StraightTrunkPlacer;
+import net.minecraft.world.gen.treedecorator.TrunkVineTreeDecorator;
+import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
+import net.minecraft.world.gen.treedecorator.LeaveVineTreeDecorator;
+import net.minecraft.world.gen.treedecorator.CocoaTreeDecorator;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.NoiseDependant;
+import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
+import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
+import net.minecraft.world.gen.feature.structure.StructureFeatures;
+import net.minecraft.world.gen.feature.TwoLayerFeature;
+import net.minecraft.world.gen.feature.SphereReplaceConfig;
+import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.feature.FeatureSpread;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.BlockClusterFeatureConfig;
+import net.minecraft.world.gen.feature.BigMushroomFeatureConfig;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.blockplacer.ColumnBlockPlacer;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.biome.SoundAdditionsAmbience;
+import net.minecraft.world.biome.ParticleEffectAmbience;
+import net.minecraft.world.biome.MoodSoundAmbience;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.biome.BiomeGenerationSettings;
+import net.minecraft.world.biome.BiomeAmbience;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.IWorldWriter;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.Direction;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.block.HugeMushroomBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.ancientvillages.AncientVillagesModElements;
+
+import java.util.Set;
+import java.util.Random;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+@AncientVillagesModElements.ModElement.Tag
+public class VillagerBiomeBiome extends AncientVillagesModElements.ModElement {
+	public static Biome biome;
+	public VillagerBiomeBiome(AncientVillagesModElements instance) {
+		super(instance, 22);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new BiomeRegisterHandler());
+	}
+	private static class BiomeRegisterHandler {
+		@SubscribeEvent
+		public void registerBiomes(RegistryEvent.Register<Biome> event) {
+			if (biome == null) {
+				BiomeAmbience effects = new BiomeAmbience.Builder().setFogColor(-10092544).setWaterColor(-13408768).setWaterFogColor(-16711936)
+						.withSkyColor(-10092544).withFoliageColor(-6750208).withGrassColor(-13421824)
+						.setMoodSound(new MoodSoundAmbience((net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+								.getValue(new ResourceLocation("ambient.crimson_forest.mood")), 6000, 8, 2))
+						.setAdditionsSound(new SoundAdditionsAmbience((net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+								.getValue(new ResourceLocation("ambient.crimson_forest.additions")), 0.0111D))
+						.setParticle(new ParticleEffectAmbience(ParticleTypes.WHITE_ASH, 0.005f)).build();
+				BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder()
+						.withSurfaceBuilder(SurfaceBuilder.DEFAULT.func_242929_a(new SurfaceBuilderConfig(Blocks.MAGMA_BLOCK.getDefaultState(),
+								Blocks.LAVA.getDefaultState(), Blocks.LAVA.getDefaultState())));
+				biomeGenerationSettings.withStructure(StructureFeatures.MINESHAFT);
+				biomeGenerationSettings.withStructure(StructureFeatures.VILLAGE_DESERT);
+				biomeGenerationSettings.withStructure(StructureFeatures.JUNGLE_PYRAMID);
+				biomeGenerationSettings.withStructure(StructureFeatures.DESERT_PYRAMID);
+				biomeGenerationSettings.withStructure(StructureFeatures.SHIPWRECK);
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.TREE
+						.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.OAK_LOG.getDefaultState()),
+								new SimpleBlockStateProvider(Blocks.OAK_LOG.getDefaultState()),
+								new BlobFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(0), 3),
+								new StraightTrunkPlacer(7, 2, 0), new TwoLayerFeature(1, 0, 1)))
+										.setDecorators(ImmutableList.of(CustomLeaveVineTreeDecorator.instance, CustomTrunkVineTreeDecorator.instance,
+												new CustomCocoaTreeDecorator()))
+										.setMaxWaterDepth(0).build())
+						.withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT)
+						.withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(1, 0.1F, 1))));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.RANDOM_PATCH.withConfiguration(Features.Configs.GRASS_PATCH_CONFIG).withPlacement(Features.Placements.PATCH_PLACEMENT)
+								.withPlacement(Placement.COUNT_NOISE.configure(new NoiseDependant(-0.8D, 5, 3))));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.FLOWER.withConfiguration(Features.Configs.NORMAL_FLOWER_CONFIG)
+								.withPlacement(Features.Placements.VEGETATION_PLACEMENT).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT)
+								.func_242731_b(1));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.HUGE_BROWN_MUSHROOM.withConfiguration(new BigMushroomFeatureConfig(
+								new SimpleBlockStateProvider(Blocks.BROWN_MUSHROOM_BLOCK.getDefaultState().with(HugeMushroomBlock.UP, Boolean.TRUE)
+										.with(HugeMushroomBlock.DOWN, Boolean.FALSE)),
+								new SimpleBlockStateProvider(Blocks.MUSHROOM_STEM.getDefaultState().with(HugeMushroomBlock.UP, Boolean.FALSE)
+										.with(HugeMushroomBlock.DOWN, Boolean.FALSE)),
+								1)));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.HUGE_RED_MUSHROOM.withConfiguration(new BigMushroomFeatureConfig(
+								new SimpleBlockStateProvider(Blocks.RED_MUSHROOM_BLOCK.getDefaultState().with(HugeMushroomBlock.DOWN, Boolean.FALSE)),
+								new SimpleBlockStateProvider(Blocks.MUSHROOM_STEM.getDefaultState().with(HugeMushroomBlock.UP, Boolean.FALSE)
+										.with(HugeMushroomBlock.DOWN, Boolean.FALSE)),
+								1)));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.DISK
+								.withConfiguration(new SphereReplaceConfig(Blocks.SAND.getDefaultState(), FeatureSpread.func_242253_a(2, 4), 2,
+										ImmutableList.of(Blocks.MAGMA_BLOCK.getDefaultState(), Blocks.LAVA.getDefaultState())))
+								.withPlacement(Features.Placements.SEAGRASS_DISK_PLACEMENT).func_242731_b(2));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.DISK
+								.withConfiguration(new SphereReplaceConfig(Blocks.GRAVEL.getDefaultState(), FeatureSpread.func_242253_a(2, 3), 2,
+										ImmutableList.of(Blocks.MAGMA_BLOCK.getDefaultState(), Blocks.LAVA.getDefaultState())))
+								.withPlacement(Features.Placements.SEAGRASS_DISK_PLACEMENT).func_242731_b(2));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.RANDOM_PATCH.withConfiguration(
+								(new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.CACTUS.getDefaultState()),
+										new ColumnBlockPlacer(1, 2))).tries(1).func_227317_b_().build()));
+				DefaultBiomeFeatures.withCavesAndCanyons(biomeGenerationSettings);
+				DefaultBiomeFeatures.withOverworldOres(biomeGenerationSettings);
+				DefaultBiomeFeatures.withEmeraldOre(biomeGenerationSettings);
+				DefaultBiomeFeatures.withFossils(biomeGenerationSettings);
+				DefaultBiomeFeatures.withMonsterRoom(biomeGenerationSettings);
+				DefaultBiomeFeatures.withDisks(biomeGenerationSettings);
+				DefaultBiomeFeatures.withForestRocks(biomeGenerationSettings);
+				MobSpawnInfo.Builder mobSpawnInfo = new MobSpawnInfo.Builder().isValidSpawnBiomeForPlayer();
+				mobSpawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(EntityType.END_CRYSTAL, 20, 1, 1));
+				mobSpawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(EntityType.HOGLIN, 5, 1, 3));
+				mobSpawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(EntityType.HUSK, 5, 1, 6));
+				mobSpawnInfo.withSpawner(EntityClassification.AMBIENT, new MobSpawnInfo.Spawners(EntityType.ZOMBIE_HORSE, 1, 2, 2));
+				mobSpawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(EntityType.field_242287_aj, 6, 1, 4));
+				biome = new Biome.Builder().precipitation(Biome.RainType.NONE).category(Biome.Category.NONE).depth(0.1f).scale(0.2f).temperature(0.5f)
+						.downfall(0f).setEffects(effects).withMobSpawnSettings(mobSpawnInfo.copy())
+						.withGenerationSettings(biomeGenerationSettings.build()).build();
+				event.getRegistry().register(biome.setRegistryName("ancient_villages:villager_biome"));
+			}
+		}
+	}
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+		BiomeManager.addBiome(BiomeManager.BiomeType.WARM,
+				new BiomeManager.BiomeEntry(RegistryKey.getOrCreateKey(Registry.BIOME_KEY, WorldGenRegistries.BIOME.getKey(biome)), 10));
+	}
+	private static class CustomLeaveVineTreeDecorator extends LeaveVineTreeDecorator {
+		public static final CustomLeaveVineTreeDecorator instance = new CustomLeaveVineTreeDecorator();
+		public static com.mojang.serialization.Codec<LeaveVineTreeDecorator> codec;
+		public static TreeDecoratorType tdt;
+		static {
+			codec = com.mojang.serialization.Codec.unit(() -> instance);
+			tdt = new TreeDecoratorType(codec);
+			tdt.setRegistryName("villager_biome_lvtd");
+			ForgeRegistries.TREE_DECORATOR_TYPES.register(tdt);
+		}
+		@Override
+		protected TreeDecoratorType<?> func_230380_a_() {
+			return tdt;
+		}
+
+		@Override
+		protected void func_227424_a_(IWorldWriter ww, BlockPos bp, BooleanProperty bpr, Set<BlockPos> sbc, MutableBoundingBox mbb) {
+			this.func_227423_a_(ww, bp, Blocks.WEEPING_VINES.getDefaultState(), sbc, mbb);
+		}
+	}
+
+	private static class CustomTrunkVineTreeDecorator extends TrunkVineTreeDecorator {
+		public static final CustomTrunkVineTreeDecorator instance = new CustomTrunkVineTreeDecorator();
+		public static com.mojang.serialization.Codec<CustomTrunkVineTreeDecorator> codec;
+		public static TreeDecoratorType tdt;
+		static {
+			codec = com.mojang.serialization.Codec.unit(() -> instance);
+			tdt = new TreeDecoratorType(codec);
+			tdt.setRegistryName("villager_biome_tvtd");
+			ForgeRegistries.TREE_DECORATOR_TYPES.register(tdt);
+		}
+		@Override
+		protected TreeDecoratorType<?> func_230380_a_() {
+			return tdt;
+		}
+
+		@Override
+		protected void func_227424_a_(IWorldWriter ww, BlockPos bp, BooleanProperty bpr, Set<BlockPos> sbc, MutableBoundingBox mbb) {
+			this.func_227423_a_(ww, bp, Blocks.WEEPING_VINES.getDefaultState(), sbc, mbb);
+		}
+	}
+
+	private static class CustomCocoaTreeDecorator extends CocoaTreeDecorator {
+		public static final CustomCocoaTreeDecorator instance = new CustomCocoaTreeDecorator();
+		public static com.mojang.serialization.Codec<CustomCocoaTreeDecorator> codec;
+		public static TreeDecoratorType tdt;
+		static {
+			codec = com.mojang.serialization.Codec.unit(() -> instance);
+			tdt = new TreeDecoratorType(codec);
+			tdt.setRegistryName("villager_biome_ctd");
+			ForgeRegistries.TREE_DECORATOR_TYPES.register(tdt);
+		}
+		public CustomCocoaTreeDecorator() {
+			super(0.2f);
+		}
+
+		@Override
+		protected TreeDecoratorType<?> func_230380_a_() {
+			return tdt;
+		}
+
+		@Override
+		public void func_225576_a_(ISeedReader p_225576_1_, Random p_225576_2_, List<BlockPos> p_225576_3_, List<BlockPos> p_225576_4_,
+				Set<BlockPos> p_225576_5_, MutableBoundingBox p_225576_6_) {
+			if (!(p_225576_2_.nextFloat() >= 0.2F)) {
+				int i = p_225576_3_.get(0).getY();
+				p_225576_3_.stream().filter((p_236867_1_) -> {
+					return p_236867_1_.getY() - i <= 2;
+				}).forEach((p_242865_5_) -> {
+					for (Direction direction : Direction.Plane.HORIZONTAL) {
+						if (p_225576_2_.nextFloat() <= 0.25F) {
+							Direction direction1 = direction.getOpposite();
+							BlockPos blockpos = p_242865_5_.add(direction1.getXOffset(), 0, direction1.getZOffset());
+							if (Feature.isAirAt(p_225576_1_, blockpos)) {
+								BlockState blockstate = Blocks.CARROTS.getDefaultState();
+								this.func_227423_a_(p_225576_1_, blockpos, blockstate, p_225576_5_, p_225576_6_);
+							}
+						}
+					}
+				});
+			}
+		}
+	}
+}
