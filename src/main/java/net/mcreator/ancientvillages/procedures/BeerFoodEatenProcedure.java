42f85cc486ae5da609a788d509430a071fdeebae
Poopybuthole69 <Martijn@192.168.2.46>
2021-05-03 16:04:02 +0200

    Hopefulyl fix everything

--- src/main/java/net/mcreator/ancientvillages/procedures/BeerFoodEatenProcedure.java
+++ src/main/java/net/mcreator/ancientvillages/procedures/BeerFoodEatenProcedure.java
@@ -1,11 +1,19 @@
 package net.mcreator.ancientvillages.procedures;
 
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.ancientvillages.AncientVillagesModElements;
+import net.mcreator.ancientvillages.AncientVillagesMod;
+
+import java.util.Map;
+
 @AncientVillagesModElements.ModElement.Tag
 public class BeerFoodEatenProcedure extends AncientVillagesModElements.ModElement {
-
 	public BeerFoodEatenProcedure(AncientVillagesModElements instance) {
 		super(instance, 11);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -14,16 +22,12 @@ public class BeerFoodEatenProcedure extends AncientVillagesModElements.ModElemen
 				AncientVillagesMod.LOGGER.warn("Failed to load dependency entity for procedure BeerFoodEaten!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.STRENGTH, (int) 200, (int) 1, (false), (false)));
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 80, (int) 1, (false), (false)));
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA, (int) 60, (int) 1, (false), (false)));
-
 	}
-
 }
